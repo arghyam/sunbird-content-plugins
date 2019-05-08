83f539fb8b058a5be2fd5fd93687755cb3c0b3ae
NandiniAV <nandini.achutha@tarento.com>
2019-05-08 14:22:52 +0530

    JAL 319 - video file type error message

--- org.ekstep.video-1.4/editor/videoapp.js
+++ org.ekstep.video-1.4/editor/videoapp.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('videoApp', [])
-    .controller('videoCtrl', ['$scope', '$injector', 'instance', function ($scope, $injector, instance) {
+    .controller('videoCtrl', ['$scope', '$injector', 'instance', '$http', function ($scope, $injector, instance, $http) {
         var ctrl = this;
         ctrl.videoUrl = '';
         ctrl.show = 'message';
@@ -27,6 +27,8 @@ angular.module('videoApp', [])
             fileExceed: 'Video file size is exceeded'
         }
         ctrl.isPreviewPlaying = false;
+        ctrl.formaterror = false;
+        ctrl.result = '';
         function hideLoader() {
             ctrl.loading = '';
         }
@@ -34,7 +36,6 @@ angular.module('videoApp', [])
         function showLoader() {
             ctrl.loading = 'active';
         }
-
 
         function getYoutubeCb(err, res) {
             if (res && res.data.result.content) {
@@ -136,20 +137,76 @@ angular.module('videoApp', [])
         ctrl.checkProvider =  function (url, cb){
             ctrl.vidID = ctrl.checkValidYoutube(url)
             if(ctrl.vidID){
+                ctrl.formaterror = false;
                 ctrl.videoUrl = 'https://www.youtube.com/watch?v='+ctrl.vidID;
                 ctrl.provider = 'youtube';
                 cb(null, ctrl.videoUrl, ctrl.provider);
             }
             else if (url.indexOf('drive') != -1) {
+                ctrl.formaterror = false;
                 var gdrive = url.replace('/view?usp=sharing', '').replace('open?id=', 'uc?export=download&id=').replace('file/d/', 'uc?export=download&id=').replace('/edit?usp=sharing', '');
                 ctrl.videoUrl = gdrive;
                 ctrl.provider = 'googledrive'
-                cb(null, ctrl.videoUrl, ctrl.provider);
+                // cb(null, ctrl.videoUrl, ctrl.provider);
+                var reslink = url.split('id=');
+                $http({
+                    method : "GET",
+                    url : "https://www.googleapis.com/drive/v3/files/"+ reslink[1]  + "?key=" + org.ekstep.contenteditor.config.googleAPIKey
+                }).then(function mySuccess(response) {
+                    ctrl.result = response.data;
+                    console.log(ctrl.result);
+                    ctrl.checkfileformat(url, ctrl.result.mimeType);
+                }, function myError(response) {
+                    ctrl.result = response.statusText;
+                    console.log(ctrl.result);
+                });
             }
             else{
+                ctrl.formaterror = true;
                 cb(new Error('invalid url'), null, null)
             }
         };
+
+        ctrl.checkfileformat = function(link, gdriveformat) {
+            if (link.indexOf('mp4') != -1 || link.indexOf('webm') != -1 || gdriveformat === 'video/mp4' || gdriveformat === 'video/webm') {
+                ctrl.formaterror = false;
+                var videoelement = ctrl.createVideoElement(ctrl.videoUrl);
+                ecEditor.jQuery('.content .container #previewVideo').html(videoelement);
+                var video = document.getElementsByTagName('video')[0];
+                video.play()
+                    .then(function() {
+                        var scope = angular.element(ecEditor.jQuery("#addToLesson")).scope();
+                        scope.$apply(function() {
+                            ctrl.messageDiv = false;
+                            ctrl.showAddLessonBtn = true;
+                        });
+                        console.log("Valid URL:", video);
+                    })
+                    .catch(function(err) {
+                        var scope = angular.element(ecEditor.jQuery("#addToLesson")).scope();
+                        scope.$apply(function() {
+                            ctrl.show = 'error';
+                            ctrl.messageDiv = true;
+                            ctrl.showAddLessonBtn = false;
+                        });
+                        var pkgVersion = ecEditor.getService('content').getContentMeta(org.ekstep.contenteditor.api.getContext('contentId')).pkgVersion;
+                        var object = {
+                            id: org.ekstep.contenteditor.api.getContext('contentId'),
+                            ver: !_.isUndefined(pkgVersion) && pkgVersion.toString() || '0',
+                            type: 'Content'
+                        }
+                        org.ekstep.contenteditor.api.getService(ServiceConstants.TELEMETRY_SERVICE).error({"err": err.code || '', "errtype": 'CONTENT', "stacktrace": err.toString(), "pageid": ecEditor.getCurrentStage().id, "object":object, "plugin": {id: instance.manifest.id, ver: instance.manifest.ver, category: 'core'} }); 
+                        console.log("Invalid URL:", err);
+                    });
+            } else {
+                console.log('Un-supported file');
+                ctrl.formaterror = true;
+                ctrl.show = 'error';
+                ctrl.messageDiv = true;
+                ctrl.showAddLessonBtn = false;
+            }
+        };
+        
         ctrl.toastManager =  function(errType, showMsg, showBtn, errMsg){
             var scope = angular.element(ecEditor.jQuery("#addToLesson")).scope();
             scope.$safeApply(function () {
